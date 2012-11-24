7be547691d3608523a68b9a8be86a0bddb1bee9e
Raed Atoui <raed.atoui@gmail.com>
2012-11-24 11:15:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PsdLayers::Application.config.secret_token = '37c22056bf3045b79c9ad551f7c619c3a9f40a2825c93bba95b9a2e7117c98b295199b14be045af4e0d78ef83555e503ffcbf98d648609f22aef9ef30fdaae80'
